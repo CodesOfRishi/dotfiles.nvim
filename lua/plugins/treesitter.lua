b586d6dbf84e3b40ef461807c1f59646a4dff075
Rishi K. <contact.rishikmr@gmail.com>
2024-08-03 21:20:13 +0530

    nvim-treesitter: lazy-load treesitter on `BufReadPre` and `BufNewFile`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate", -- When upgrading the treesitter, all installed parsers are updated to the latest version 
-	lazy = false,
 	opts = {
 		ensure_installed = { "c", "cpp", "java", "toml", "bash", "python", "lua" },
 	},
