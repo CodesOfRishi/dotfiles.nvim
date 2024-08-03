c172d0df2fef5dac0ae2bb842413fb1ba06533e1
Rishi K. <contact.rishikmr@gmail.com>
2024-08-03 21:22:56 +0530

    nvim-treesitter: enable highlight

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate", -- When upgrading the treesitter, all installed parsers are updated to the latest version 
 	opts = {
+		highlight = { enable = true, },
 		ensure_installed = { "c", "cpp", "java", "toml", "bash", "python", "lua" },
 	},
 }
