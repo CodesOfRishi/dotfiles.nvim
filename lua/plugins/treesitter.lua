e0657baec4be8094bd1a14696f1b692a046a33d2
Rishi K. <contact.rishikmr@gmail.com>
2024-08-03 21:23:42 +0530

    nvim-treesitter: enable indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 	build = ":TSUpdate", -- When upgrading the treesitter, all installed parsers are updated to the latest version 
 	opts = {
 		highlight = { enable = true, },
+		indent = { enable = true, },
 		ensure_installed = { "c", "cpp", "java", "toml", "bash", "python", "lua" },
 	},
 }
