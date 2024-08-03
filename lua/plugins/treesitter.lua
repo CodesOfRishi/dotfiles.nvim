139f3ec26c328364544ab61c4af8c5b5a460f723
Rishi K. <contact.rishikmr@gmail.com>
2024-08-03 21:16:29 +0530

    nvim-treesitter: update installed parsers, when treesitter is upgraded

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate", -- When upgrading the treesitter, all installed parsers are updated to the latest version 
 	lazy = false,
 	opts = {
 		ensure_installed = { "c", "cpp", "java", "toml", "bash", "python", "lua" },
