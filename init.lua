87a6580c81527a786e4b622a35c8c7c9689291b0
Rishi K. <contact.rishikmr@gmail.com>
2024-08-04 11:55:34 +0530

    Refactor Lazy.nvim configurations
    - Firstly bootstrap Lazy.nvim (in init.lua)
    - Load options, autocmds and then lazy.nvim

--- init.lua
+++ init.lua
@@ -7,11 +7,34 @@
 ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝ Rishi K.
 --]]
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
+
+-- This is also a good place to setup other settings (vim.opt)
 -- load nvim configurations modules
 local nvim_configuration_modules = { 
-	"config.lazy",
 	"config.options",
-	"config.autocmds"
+	"config.autocmds",
+	"config.lazy"
 }
 
 for i = 1, #nvim_configuration_modules, 1 do
