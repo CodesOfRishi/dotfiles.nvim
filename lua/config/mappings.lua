8f86693872946f87a5fc444bae97547bcd10a0b3
Rishi K. <contact.rishikmr@gmail.com>
2024-08-09 11:00:08 +0530

    refactor(mappings): Refactor Neovim keymap configuration for improved readability and maintainability
    - Create local 'keymap' function to wrap vim.api.nvim_set_keymap()
    - Implement shared 'opts' variable for common keymap options
    - Replace all direct vim.api.nvim_set_keymap() calls with new 'keymap' function
    - Simplify syntax for creating new keymaps throughout the configuration

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,17 +1,19 @@
-local opts = { noremap = true, silent = true }
+local function keymap(mode, key, command)
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_set_keymap(mode, key, command, opts)
+end
 
 -- INFO: CTRL-j & CTRL-k to move selected lines down & up respectively, in visual mode
-vim.api.nvim_set_keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", opts)
-vim.api.nvim_set_keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", opts)
+keymap('v', '<C-j>', ":m '>+1<CR>gv=gv")
+keymap('v', '<C-k>', ":m '<-2<CR>gv=gv")
 
 -- INFO: CTRL-j & CTRL-k to move current lines down & up respectively, in insert and normal mode
-vim.api.nvim_set_keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi", opts)
-vim.api.nvim_set_keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi", opts)
-vim.api.nvim_set_keymap('n', '<C-j>', ":m .+1<CR>==", opts)
-vim.api.nvim_set_keymap('n', '<C-k>', ":m .-2<CR>==", opts)
+keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi")
+keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi")
+keymap('n', '<C-j>', ":m .+1<CR>==")
+keymap('n', '<C-k>', ":m .-2<CR>==")
 
 -- INFO: Select all
-vim.api.nvim_set_keymap('v', '<C-a>', "<Esc>gg0vG$", opts)
-vim.api.nvim_set_keymap('i', '<C-a>', "<Esc>gg0vG$", opts)
-vim.api.nvim_set_keymap('n', '<C-a>', "<Esc>gg0vG$", opts)
-
+keymap('v', '<C-a>', "<Esc>gg0vG$")
+keymap('i', '<C-a>', "<Esc>gg0vG$")
+keymap('n', '<C-a>', "<Esc>gg0vG$")
