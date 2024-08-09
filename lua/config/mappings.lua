2d00770911025b3d4e47cb3bac53b3cf6b2e98de
Rishi K. <contact.rishikmr@gmail.com>
2024-08-09 09:54:56 +0530

    refactor(mappings): Refactor Neovim keymaps to use shared options variable
    - Create local 'opts' variable with common keymap options.
    - Replace inline options with 'opts' in all vim.api.nvim_set_keymap() calls.
    - Improve consistency and maintainability of keymap configurations.

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,15 +1,17 @@
+local opts = { noremap = true, silent = true }
+
 -- INFO: CTRL-j & CTRL-k to move selected lines down & up respectively, in visual mode
-vim.api.nvim_set_keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", opts)
+vim.api.nvim_set_keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", opts)
 
 -- INFO: CTRL-j & CTRL-k to move current lines down & up respectively, in insert and normal mode
-vim.api.nvim_set_keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', ":m .+1<CR>==", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', ":m .-2<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi", opts)
+vim.api.nvim_set_keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi", opts)
+vim.api.nvim_set_keymap('n', '<C-j>', ":m .+1<CR>==", opts)
+vim.api.nvim_set_keymap('n', '<C-k>', ":m .-2<CR>==", opts)
 
 -- INFO: Select all
-vim.api.nvim_set_keymap('v', '<C-a>', "<Esc>gg0vG$", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-a>', "<Esc>gg0vG$", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-a>', "<Esc>gg0vG$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-a>', "<Esc>gg0vG$", opts)
+vim.api.nvim_set_keymap('i', '<C-a>', "<Esc>gg0vG$", opts)
+vim.api.nvim_set_keymap('n', '<C-a>', "<Esc>gg0vG$", opts)
 
