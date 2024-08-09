7192471752938e7e75809783483a278018d8f85d
Rishi K. <contact.rishikmr@gmail.com>
2024-08-09 11:35:23 +0530

    perf(mappings): use `vim.keymap.set` function instead of `vim.api.nvim_set_keymap`
    - 'vim.keymap.set' is a newer, higher-level function introduced in Neovim 0.5.
    - 'vim.api.nvim_set_keymap' is the traditional Neovim API function for setting key mappings.
    - If using Neovim 0.5 or later, it's recommended to use vim.keymap.set for its simplicity and advanced features.
    - If there's a need to support earlier Neovim versions or require more fine-grained control, use 'vim.api.nvim_set_keymap'.
    - In general, 'vim.keymap.set' is the preferred choice for most use cases, as it provides a more concise and expressive way to define key mappings.

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,19 +1,15 @@
 local function keymap(mode, key, command)
 	local opts = { noremap = true, silent = true }
-	vim.api.nvim_set_keymap(mode, key, command, opts)
+	vim.keymap.set(mode, key, command, opts)
 end
 
--- INFO: CTRL-j & CTRL-k to move selected lines down & up respectively, in visual mode
-keymap('v', '<C-j>', ":m '>+1<CR>gv=gv")
-keymap('v', '<C-k>', ":m '<-2<CR>gv=gv")
-
--- INFO: CTRL-j & CTRL-k to move current lines down & up respectively, in insert and normal mode
-keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi")
-keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi")
+-- INFO: CTRL-j & CTRL-k to move line(s) down or up respectively in normal, visual and insert modes
 keymap('n', '<C-j>', ":m .+1<CR>==")
 keymap('n', '<C-k>', ":m .-2<CR>==")
+keymap('v', '<C-j>', ":m '>+1<CR>gv=gv")
+keymap('v', '<C-k>', ":m '<-2<CR>gv=gv")
+keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi")
+keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi")
 
 -- INFO: Select all
-keymap('v', '<C-a>', "<Esc>gg0vG$")
-keymap('i', '<C-a>', "<Esc>gg0vG$")
-keymap('n', '<C-a>', "<Esc>gg0vG$")
+keymap({ 'n', 'v', 'i' }, '<C-a>', "<Esc>gg0vG$")
