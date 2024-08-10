14cf07c45d787764f00779956258b1a3ef3790cb
Rishi K. <contact.rishikmr@gmail.com>
2024-08-10 10:53:59 +0530

    docs(mappings): add and refactor informational comments

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,7 +3,11 @@ local function keymap(mode, key, command)
 	vim.keymap.set(mode, key, command, opts)
 end
 
--- INFO: CTRL-j & CTRL-k to move line(s) down or up respectively in normal, visual and insert modes
+-- ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
+-- ╎ NOTE: Mappings for Gitsign plugin can be found in lua/plugins/gitsign.lua ╎
+-- └╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
+
+-- CTRL-j & CTRL-k to move line(s) down or up respectively in normal, visual and insert modes
 keymap('n', '<C-j>', ":m .+1<CR>==")
 keymap('n', '<C-k>', ":m .-2<CR>==")
 keymap('v', '<C-j>', ":m '>+1<CR>gv=gv")
@@ -11,10 +15,10 @@ keymap('v', '<C-k>', ":m '<-2<CR>gv=gv")
 keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi")
 keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi")
 
--- INFO: Select all
+-- Select all
 keymap({ 'n', 'v', 'i' }, '<C-a>', "<Esc>gg0vG$")
 
--- -- INFO: Resizing windows
+-- Resizing windows
 keymap('n', '<C-down>', ':resize +3<CR>')
 keymap('n', '<C-up>', ':resize -3<CR>')
 keymap('n', '<C-right>', ':vertical resize +3<CR>')
