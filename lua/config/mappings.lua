c96ae6bcffdedd02347cd567a1a8e9dc975ee4ff
Rishi K. <contact.rishikmr@gmail.com>
2024-08-11 17:50:26 +0530

    conf(mappings): Refactor keymap function for greater flexibility
    - Modify keymap function to accept an opts table instead of a single desc parameter
    - Set default values for noremap and silent within the function
    - Update existing keymaps to use new opts table format
    - Maintain existing functionality while improving extensibility

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,6 +1,8 @@
-local function keymap(mode, key, command, desc)
-	desc = desc or ""
-	vim.keymap.set(mode, key, command, { noremap = true, silent = false, desc = desc })
+local function keymap(mode, key, command, opts)
+	opts = opts or {}
+	opts.noremap = true
+	opts.silent = false
+	vim.keymap.set(mode, key, command, opts)
 end
 
 -- ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
@@ -8,26 +10,26 @@ end
 -- └╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
 
 -- CTRL-j & CTRL-k to move line(s) down or up respectively in normal, visual and insert modes
-keymap('n', '<C-j>', ":m .+1<CR>==", "Move the current line down in Normal mode")
-keymap('n', '<C-k>', ":m .-2<CR>==", "Move the current line up in Normal mode")
-keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", "Move the selected line(s) down")
-keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", "Move the selected line(s) up.")
-keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi", "Move the current line down in Insert mode")
-keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi", "Move the current line up in Insert mode")
+keymap('n', '<C-j>', ":m .+1<CR>==", { desc = "Move the current line down in Normal mode" })
+keymap('n', '<C-k>', ":m .-2<CR>==", { desc = "Move the current line up in Normal mode" })
+keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", { desc = "Move the selected line(s) down" })
+keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", { desc = "Move the selected line(s) up" })
+keymap('i', '<C-j>', "<Esc>:m .+1<CR>==gi", { desc = "Move the current line down in Insert mode" })
+keymap('i', '<C-k>', "<Esc>:m .-2<CR>==gi", { desc = "Move the current line up in Insert mode" })
 
 -- Select all
-keymap({ 'n', 'v', 'i' }, '<C-a>', "<Esc>gg0vG$", "Select all text in Normal/Visual/Insert modes")
+keymap({ 'n', 'v', 'i' }, '<C-a>', "<Esc>gg0vG$", { desc = "Select all text in Normal/Visual/Insert modes" })
 
 -- Resizing windows
-keymap('n', '<C-down>', ':resize +3<CR>', "Increase current window height by 3")
-keymap('n', '<C-up>', ':resize -3<CR>', "Decrease current window height by 3")
-keymap('n', '<C-right>', ':vertical resize +3<CR>', "Increase current window width by 3")
-keymap('n', '<C-left>', ':vertical resize -3<CR>', "Decrease current window width by 3")
+keymap('n', '<C-down>', ':resize +3<CR>', { desc = "Increase current window height by 3" })
+keymap('n', '<C-up>', ':resize -3<CR>', { desc = "Decrease current window height by 3" })
+keymap('n', '<C-right>', ':vertical resize +3<CR>', { desc = "Increase current window width by 3" })
+keymap('n', '<C-left>', ':vertical resize -3<CR>', { desc = "Decrease current window width by 3" })
 
 -- better indentation
 -- allows you to shift selected text to the right or left using the > and < keys in visual mode, while maintaining the original selection.
-keymap('v', '>', '>gv', "right indentation")
-keymap('v', '<', '<gv', "left indentation")
+keymap('v', '>', '>gv', { desc = "right indentation" })
+keymap('v', '<', '<gv', { desc = "left indentation" })
 
 -- which-key.nvim key mappings
-keymap('n', '<leader>?', function() require("which-key").show({global = false}) end, "Buffer local keymaps (which-key)")
+keymap('n', '<leader>?', function() require("which-key").show({global = false}) end, { desc = "Buffer local keymaps (which-key)" })
