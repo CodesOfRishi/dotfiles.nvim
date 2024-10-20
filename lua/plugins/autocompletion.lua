13cfef5f933be8af0a780bfbd25cc511398c284f
Rishi K. <contact.rishikmr@gmail.com>
2024-10-20 21:18:23 +0530

    feat(plugin): disable 'hrsh7th/nvim-cmp', enable 'Saghen/blink.cmp'
    Configuration of 'nvim-cmp' is backed in 'autocompletion.lua.bak'.

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,102 +1,36 @@
 return {
-	-- {
-	-- 	"saadparwaiz1/cmp_luasnip",
-	-- 	dependencies={ "L3MON4D3/LuaSnip", "hrsh7th/nvim-cmp" },
-	-- 	enabled = false,
-	-- },
-	{
-		"hrsh7th/cmp-buffer",
-		dependencies = { "hrsh7th/nvim-cmp" },
-	},
-	{
-		"hrsh7th/cmp-path",
-		dependencies = { "hrsh7th/nvim-cmp" },
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require'cmp'
+	'saghen/blink.cmp',
+	event = "BufReadPre", -- lazy loading handled internally
+	-- use a release tag to download pre-built binaries
+	version = 'v0.*',
+	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- build = 'cargo build --release',
+	-- If you use nix, you can build from source using latest nightly rust with:
+	-- build = 'nix run .#build-plugin',
 
-			-- snippet = {
-			-- 	expand = function(args)
-			-- 		require'luasnip'.lsp_expand(args.body)
-			-- 	end,
-			-- },
-			cmp.setup({
-				mapping = {
-					-- when in Insert mode & menu is on display, hit Tab to select next item
-					['<Tab>'] = cmp.mapping(
-						function(fallback)
-							if cmp.visible() then
-								cmp.select_next_item()
-							else
-								fallback()
-							end
-						end, { "i" }
-					),
-					-- when in Insert mode & menu is on display, hit Shift+Tab to select previous item
-					['<S-Tab>'] = cmp.mapping(
-						function(fallback)
-							if cmp.visible() then
-								cmp.select_prev_item()
-							else
-								fallback()
-							end
-						end, { "i" }
-					),
-					-- when in Insert mode & menu is on diplay, hit Ctrl+j to scroll the documentation window downward
-					['<C-j>'] = cmp.mapping(
-						function(fallback)
-							if cmp.visible() then
-								cmp.scroll_docs(4)
-							else
-								fallback()
-							end
-						end, { "i" }
-					),
-					-- when in Insert mode & menu is on diplay, hit Ctrl+k to scroll the documentation window upward
-					['<C-k>'] = cmp.mapping(
-						function(fallback)
-							if cmp.visible() then
-								cmp.scroll_docs(-4)
-							else
-								fallback()
-							end
-						end, { "i" }
-					),
-					-- when in Insert mode & menu is on display, hit Enter to accepts the currently selected completion item.
-					['<CR>'] = cmp.mapping(
-						function(fallback)
-							if cmp.visible() then
-								cmp.confirm({  select = true  })
-							else 
-								fallback()
-							end
-						end, { "i" }
-					),
-					-- when in Insert mode, invoke completion
-					['<C-c>'] = cmp.mapping(function() cmp.complete() end, { "i" }),
-					-- when the completion menu is on display, close it
-					['<C-q>'] = cmp.mapping(
-						function(fallback)
-							if cmp.visible() then
-								cmp.abort()
-							else
-								fallback()
-							end
-						end
-					),
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				sources = cmp.config.sources({
-					-- { name = "luansip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
-		end,
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		highlight = {
+			-- sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- useful for when your theme doesn't support blink.cmp
+			-- will be removed in a future release, assuming themes add support
+			use_nvim_cmp_as_default = true,
+		},
+		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+		-- adjusts spacing to ensure icons are aligned
+		nerd_font_variant = 'normal',
+		keymap = {
+			show = '<C-c>',
+			hide = '<C-q>',
+			accept = '<CR>',
+			-- select_and_accept = {},
+			select_prev = { '<Up>', '<S-Tab>' },
+			select_next = { '<Down>', '<Tab>' },
+			show_documentation = {},
+			hide_documentation = {},
+			scroll_documentation_up = '<C-k>',
+			scroll_documentation_down = '<C-j>',
+		}
 	}
 }
