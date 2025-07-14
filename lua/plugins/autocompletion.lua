efd225420add536e3f4711ea36fe140196bbd450
Rishi K. <contact.rishikmr@gmail.com>
2025-07-14 07:09:29 +0530

    conf(plugin): update & configure 'Saghen/blink.cmp' to version `v1.*`

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -2,7 +2,7 @@ return {
 	'saghen/blink.cmp',
 	event = "BufReadPre", -- lazy loading handled internally
 	-- use a release tag to download pre-built binaries
-	version = 'v0.*',
+	version = '1.*',
 	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 	-- build = 'cargo build --release',
 	-- If you use nix, you can build from source using latest nightly rust with:
@@ -10,27 +10,45 @@ return {
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
+	---
 	opts = {
-		highlight = {
-			-- sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release, assuming themes add support
-			use_nvim_cmp_as_default = true,
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = 'mono'
 		},
-		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- adjusts spacing to ensure icons are aligned
-		nerd_font_variant = 'normal',
+
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = { 
+			documentation = { auto_show = false },
+
+			-- Display a preview of the selected item on the current line
+			ghost_text = { enabled = true },
+		},
+
+		-- Default list of enabled providers defined so that you can extend it 
+		-- elsewhere in config, without redefining it, due to 'opts_extend' 
+		sources = {
+			-- default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { 'path', 'buffer' },
+		},
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 		keymap = {
-			show = '<C-c>',
-			hide = '<C-q>',
-			accept = '<CR>',
-			-- select_and_accept = {},
-			select_prev = { '<Up>', '<S-Tab>' },
-			select_next = { '<Down>', '<Tab>' },
-			show_documentation = {},
-			hide_documentation = {},
-			scroll_documentation_up = '<C-k>',
-			scroll_documentation_down = '<C-j>',
-		}
-	}
+			['<C-c>'] = { 'show' },
+			['<C-q>'] = { 'hide' },
+			['<CR>'] = { 'accept' },
+			['<Up>'] = { 'select_prev' },
+			['<S-Tab>'] = { 'select_prev' },
+			['<Down>'] = { 'select_next' },
+			['<Tab>'] = { 'select_next' },
+			['<C-k>'] = { 'scroll_documentation_up' },
+			['<C-j>'] = { 'scroll_documentation_down' },
+		},
+	},
+	-- opts_extend = { "sources.default" }
 }
