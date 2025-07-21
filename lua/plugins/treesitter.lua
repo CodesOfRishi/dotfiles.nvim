d02791e0857492ce9a13b28b14eec6855fd45c3e
Rishi K. <contact.rishikmr@gmail.com>
2025-07-21 08:50:59 +0530

    fix(plugin): fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,11 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate", -- When upgrading the treesitter, all installed parsers are updated to the latest version 
-	opts = {
-		highlight = { enable = true, },
-		indent = { enable = true, },
-		ensure_installed = { "c", "cpp", "java", "toml", "bash", "python", "lua", "html" },
-	},
-	enabled = false,
+	config = function()
+		require("nvim-treesitter.configs").setup {
+			ensure_installed = { "c", "cpp", "toml", "bash", "python", "lua", "html" },
+			indent = { enable = true, },
+			highlight = { enable = true, },
+		}
+	end,
 }
