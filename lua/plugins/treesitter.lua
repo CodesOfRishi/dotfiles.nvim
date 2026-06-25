bf61bf4df389bcc306a2dc64f6284735bafb85ad
Rishi K. <contact.rishikmr@gmail.com>
2026-06-25 16:37:28 +0530

    conf(plugin): switch to 'main' branch and use `opts` instead of `config` in Lazy config
    - Switch to using using 'main' branch instead of 'master'.
    - Use the 'opts' option instead of 'config' in Lazy configuration.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,11 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
+	branch = "main",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate", -- When upgrading the treesitter, all installed parsers are updated to the latest version 
-	config = function()
-		require("nvim-treesitter.configs").setup {
+	opts = {
 			ensure_installed = { "c", "cpp", "toml", "bash", "python", "lua", "html" },
 			indent = { enable = true, },
 			highlight = { enable = true, },
-		}
-	end,
+	},
 }
