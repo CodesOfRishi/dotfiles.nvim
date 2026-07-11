9a2041435032039f409bb001bdf597c73f8ea76c
Rishi K. <contact.rishikmr@gmail.com>
2026-07-11 06:40:50 +0000

    feat(plugin): ensure 'toml' treesitter parser is installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 	cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
 	build = ":TSUpdate",
 	config = function()
-		local ensure_installed = { "c", "cpp", "bash", "python", "lua", "vimdoc" }
+		local ensure_installed = { "c", "cpp", "bash", "python", "lua", "vimdoc", "toml" }
 		local installed = require("nvim-treesitter.config").get_installed() -- or .installed_parsers()
 		local to_install = vim.tbl_filter(function(lang)
 			return not vim.tbl_contains(installed, lang)
