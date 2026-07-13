328c0da336fe49133951912fb72529a7bb4bb7ba
Rishi K. <contact.rishikmr@gmail.com>
2026-07-13 13:28:50 +0530

    feat(mappings): add a key mapping for executing the current file in a split terminal
    - Add '<leader>xx' mapping for executing the current file in a split terminal, in normal mode.

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -48,3 +48,6 @@ keymap({ "n", "x" }, "<Down>", function() return vim.v.count == 0 and "g<Down>" 
 
 -- which-key.nvim key mappings
 keymap('n', '<leader>?', function() require("which-key").show({global = false}) end, { desc = "Buffer local keymaps (which-key)" })
+
+-- execute current file in a split terminal
+keymap('n', '<leader>xx', ':split<CR>:terminal ./%<CR>', { desc = "Execuate current file in a split terminal" })
